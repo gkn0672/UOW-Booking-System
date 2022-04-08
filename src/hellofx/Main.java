621f61cfdee039b5d087ce8e520f8f217b4129c4
Agu <gkn060702@gmail.com>
2022-04-08 15:59:20 +0800

    first commit

---
+++ src/hellofx/Main.java
@@ -0,0 +1,22 @@
+package hellofx;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    
+    @Override
+    public void start(Stage PrimaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("Login_form.fxml"));
+        PrimaryStage.setTitle("UOW room booking system");
+        PrimaryStage.setScene(new Scene(root, 600, 400));
+        PrimaryStage.show();
+    }
+
+    public static void main(String[] args){
+        launch(args);
+    }
+}
