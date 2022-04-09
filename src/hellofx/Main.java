f53034c7874076a830099d91dae709c85a8a864c
Agu <gkn060702@gmail.com>
2022-04-09 08:17:24 +0800

    update 2

--- src/hellofx/Main.java
+++ src/hellofx/Main.java
@@ -13,8 +13,13 @@ public class Main extends Application {
     private static Stage stg;
 
     @Override
+    //Show Login form
     public void start(Stage PrimaryStage) throws Exception{      
+
+        //Init Xampp connection
         MysqlConnect c1 = new MysqlConnect("uow_booking_system", "root", "");
+
+        //Login form settings
         stg = PrimaryStage;
         PrimaryStage.setResizable(false);
         Parent root = FXMLLoader.load(getClass().getResource("Login_form.fxml"));
@@ -23,10 +28,13 @@ public class Main extends Application {
         PrimaryStage.show();
     }
 
+    //Change the scene
     public void changeScene(String fxml) throws IOException{
         Parent pane = FXMLLoader.load(getClass().getResource(fxml));
         stg.getScene().setRoot(pane);
     }
+
+    //Main method
     public static void main(String[] args){
         launch(args);
     }
--- src/hellofx/MysqlConnect.java
+++ src/hellofx/MysqlConnect.java
@@ -6,24 +6,37 @@ import javafx.scene.control.Alert.AlertType;
 
 public class MysqlConnect {
     private Connection conn;
+    static String template = "jdbc:mysql://localhost/%s?useEncoding=true&characterEncoding=UTF-8&user=%s&password=%s";
+
+    //Default constructor
     public MysqlConnect(String database, String user, String password){
         conn = null;
         connect(database, user, password);
     }
 
-    static String template = "jdbc:mysql://localhost/%s?useEncoding=true&characterEncoding=UTF-8&user=%s&password=%s";
-
+    //Establish connection
     public void connect(String database, String user, String password) {
         try {
             Class.forName("com.mysql.jdbc.Driver").newInstance();
             conn = DriverManager.getConnection(String.format(template, database, user, password));
             //...
         } catch (Exception ex) {
+
+            //Display error message
             Alert alert = new Alert(AlertType.ERROR);
             alert.setHeaderText("Make sure that Xampp is running");
             alert.setContentText(ex.getMessage());  
             alert.showAndWait();
+
+            //Force exit
             System.exit(0);
         }
     }
+/*
+    public void insert(){
+
+    }
+
+    public void 
+    */
 }
