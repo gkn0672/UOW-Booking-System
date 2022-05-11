7580a540f3a4dcc80f7c20fccb19402bb966c06b
bhh97 <hbohao@outlook.com>
2022-05-11 14:47:10 +0800

    A

---
+++ src/hellofx/Book_room.java
@@ -0,0 +1,150 @@
+package hellofx;
+
+import java.io.IOException;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.util.ResourceBundle;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+
+public class Book_room extends Student{
+    private Main m;
+    private Student s;
+    private String id;
+    public Book_room (String username, String role, Main m, String id, Student s){
+        super(username, role);
+        this.m = m;
+        this.id = id;
+        this.s = s;
+    }
+
+public void initialize(URL arg0, ResourceBundle arg1){
+    try{
+        Connection con = m.getC().getConnection();
+        PreparedStatement ps;
+        ResultSet rs;
+        ps = con.prepareStatement("SELECT `Code_name` FROM `room_promo` WHERE `RoomID` = ?");
+        ps.setString(1, String.valueOf(id));
+        rs = (ResultSet) ps.executeQuery();
+
+        while(rs.next()){
+            Activelist.getItems().add(rs.getString(1));
+        }
+        Confirmbooking.setDisable(true);
+    }
+    catch(Exception e){
+        e.printStackTrace();
+    }
+}
+
+//Event trigger (Active promo code check)
+public void Checkactive(ActionEvent event){
+    String value = Activelist.getValue();
+
+    if(!value.equals("")){
+        Confirmbooking.setDisable(false);
+    }
+    else{
+        Confirmbooking.setDisable(true);
+    }
+}
+
+@FXML
+private ComboBox<String> Activelist;
+
+@FXML
+private Button Cancel;
+
+@FXML
+private Button Confirmbooking;
+
+@FXML
+void Cancelbooking(ActionEvent event) throws Exception{
+    cancel(Cancel);
+}
+
+
+
+@FXML
+void Confirmbooking(ActionEvent event) throws Exception{
+    Connection con = m.getC().getConnection();
+    PreparedStatement ps;
+    PreparedStatement ps1;
+    PreparedStatement ps2;
+    PreparedStatement ps3;
+    double discountPrice = 0.0;
+    double originalPrice = 0.0;
+    int promoCode = 0;
+    int afterDiscount = 0;
+
+    //Get promotion code 
+    ps1 = con.prepareStatement("SELECT `Code_sale` FROM `promotioncode` WHERE `Code_name` = ?");
+    ps1.setString(1,Activelist.getValue());
+    ResultSet rs = (ResultSet) ps1.executeQuery();
+    if(rs.next()){
+        promoCode = rs.getInt(1);
+    }
+
+    //Get original price
+    ps2 = con.prepareStatement("SELECT * FROM `room` WHERE `ID` = ?");
+    ps2.setString(1, String.valueOf(id));
+    rs = ps2.executeQuery();
+    if(rs.next()){
+        originalPrice = rs.getDouble(9);
+    }
+
+    //get discount price
+    afterDiscount = 100 - promoCode;
+    discountPrice = (afterDiscount * originalPrice) / 100;
+
+    //Update the discountprice in the database
+    ps3 = con.prepareStatement("UPDATE `room` SET `discountPrice` = ? WHERE `ID` = ?");
+    ps3.setDouble(1,discountPrice);
+    ps3.setString(2, String.valueOf(id));
+    ps3.execute();
+
+
+    
+    //Set selected room status to "Unavailable"
+    ps = con.prepareStatement("UPDATE `room` SET `Status` = ? WHERE `ID` = ?");
+    ps.setString(1, "Not available");
+    ps.setString(2, String.valueOf(id));
+    ps.execute();
+
+    //Update room available
+    if(ps.executeUpdate() != 0){
+    s.updateRoom();
+    cancel(Cancel);
+    Main m = new Main();
+    FXMLLoader loader = new FXMLLoader(getClass().getResource("Layout/Success_message.fxml"));
+    Success_message sm1 = new Success_message("Booking Successful");
+    loader.setController(sm1);
+    m.popup(loader, "Success", 332, 194, 650, 250);  
+    s.updateBookingList(Integer.parseInt(id));
+    }
+
+    }
+}
+
+    
+
+
+
+
+
+
+
+
